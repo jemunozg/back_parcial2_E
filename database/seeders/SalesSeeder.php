b0d95de2d76e7c79fe7a8e9ec8ad9a782c54d32c
jemunozg <jemunozg@unal.edu.co>
2023-05-08 12:17:59 -0500

    seeders sales

--- database/seeders/SalesSeeder.php
+++ database/seeders/SalesSeeder.php
@@ -14,6 +14,31 @@ class SalesSeeder extends Seeder
      */
     public function run()
     {
+        sales::create([
+            'nombre_vendedor' => 'Yorgs',
+            'nombre_cliente' =>' Estiven',
+            'nombre_producto' =>' Camiseta',
+            'precio' => 50000,
+            'fecha_compra' =>' 08/05/2023',
+        ]);
+
+        sales::create([
+            'nombre_vendedor' => 'Nicolas',
+            'nombre_cliente' => 'Laura',
+            'nombre_producto' =>' Falda',
+            'precio' => 60000,
+            'fecha_compra' =>' 07/05/2023',
+        ]);
+
+        
+        sales::create([
+            'nombre_vendedor' => 'Jhon',
+            'nombre_cliente' => 'Sofia',
+            'nombre_producto' =>' Zapatos',
+            'precio' => 90000,
+            'fecha_compra' =>' 06/05/2023',
+        ]);
+
         
     }
 }
